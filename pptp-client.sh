9836b2e4514af95759f5ae5505ce8c25241ca8b0
linuxteknosia <54249332+linuxteknosia@users.noreply.github.com>
2019-08-20 20:03:55 +0700

    Create pptp-client.sh

---
+++ pptp-client.sh
@@ -0,0 +1,24 @@
+echo "+================================+"
+echo "+ pptp client command line linux +"
+echo "+================================+"
+#mobprobe ppp-generic
+#apt-get install pptp-linux pptpd ppp curl
+cat <<EOF > /etc/ppp/peers/dial-vpn
+pty "pptp (ip address or host vpn server ) --nolaunchpppd"
+lock
+noauth
+nobsdcomp
+nodeflate
+name ( username vpn )
+remotename dial-vpn
+ipparam dial-vpn
+require-mppe-128
+usepeerdns
+defaultroute
+persist
+EOF
+cat <<EOF > /etc/ppp/chap-secrets
+username_vpn   dial-vpn        password_vpn
+EOF
+echo " You can connect vpn using " pon dial-vpn ""
+echo " You can disconect vpn using " poff dial-vpn ""
